97c40b943e77bc90887dbf5fdce943538da65a54
santiago <carlossantiagocalligari@gmail.com>
2022-04-03 21:50:45 -0300

    Funciona el listen, a punto de hacer el cliente.

--- srvFtp/main.c
+++ srvFtp/main.c
@@ -4,25 +4,23 @@
 #include <sys/types.h>
 #include <string.h>
 #include <netdb.h>
-
 #define BACKLOG 1 //numero de conexiones permitidas al mismo tiempo.
 
 int main(int argc, char *argv[])
 {
 
 	//SOCKFD socket escucha, newfd socket conexion, len mensaje
-	//their_add direccion de cliente, res resultado de addrinfo
-	int sockfd, new_fd, len, bytes_sent;  
-	char *puerto, *msg;
+	//their_add direccion de cliente, res resultado deb addrinfo
+	int sockfd,  new_fd, len, bytes_sent;  
+	char *msg, *puerto;
 	struct sockaddr_storage their_addr;
 	socklen_t addr_size;
 	struct addrinfo hints, *res;
-
+as
 
 	//hints seteado a 0, puerto igualado a lo enviado en cli
-	memset(&hints,0, sizeof hints);
+	memset(&hints,0 , sizeof hints);
 	puerto = argv[1];
-
 	hints.ai_family = AF_UNSPEC;    //usar ipv4 o ipv6
 	hints.ai_socktype = SOCK_STREAM;//tcp
 	hints.ai_flags = AI_PASSIVE;	//llenar ip
@@ -32,18 +30,15 @@ int main(int argc, char *argv[])
 	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol); //HACEMOS EL SOCKET QUE VAMOS A ESCUCHAR
 
 	bind(sockfd, res->ai_addr, res->ai_addrlen); // asociamos el puerto con el programa
+	while(1){
 	listen(sockfd, BACKLOG); // escuchamos al puerto que asociamos
-
+	}
 	addr_size = sizeof their_addr; 
 	
 
 	//esto esta mal
-	int true = 1;
-	while(true)
-	{
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size); //reconocemos la existencia del cliente.
-		if(new_fd == -1) true = 0;
-	}
+	new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size); //reconocemos la existencia del cliente.
+	
 	msg = "Estamos conectados loqui";
 	len = strlen(msg);
 	bytes_sent = send(sockfd, msg, len, 0);
