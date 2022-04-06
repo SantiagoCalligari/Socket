c72eb177297b97ddac078bc60ba7075dbca1bd6a
santiago <carlossantiagocalligari@gmail.com>
2022-04-05 23:01:41 -0300

    Cree unas funciones nuevas para que el codigo no sea tan asqueroso

--- srvFtp/main.c
+++ srvFtp/main.c
@@ -7,56 +7,49 @@
 #include <netdb.h>
 #define BACKLOG 1 //numero de conexiones permitidas al mismo tiempo.
 
-int main(int argc, char *argv[])
+
+int socketbind(char *puerto)
 {
-	//SOCKFD socket escucha, newfd socket conexion, len mensaje
-	//their_add direccion de cliente, res resultado deb addrinfo
-    int sockfd,  new_fd, len, bytes_sent, rv;  
-	char *msg, *puerto;
-	struct sockaddr_storage their_addr;
-	socklen_t sin_size;
-	struct addrinfo hints, *res, *p;
+    struct addrinfo hints, *res, *p;
+    int sock, rv;
     int yes = 1;
-	//hints seteado a 0, puerto igualado a lo enviado en cli
-	memset(&hints,0 , sizeof hints);
-	puerto = argv[1];
-	hints.ai_family = AF_UNSPEC;    //usar ipv4 o ipv6
-	hints.ai_socktype = SOCK_STREAM;//tcp
-	hints.ai_flags = AI_PASSIVE;	//llenar ip
 
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;    //usar ipv4 o ipv6
+    hints.ai_socktype = SOCK_STREAM;//tcp
+    hints.ai_flags = AI_PASSIVE;	//llenar ip
 
-	if(rv = getaddrinfo(NULL, puerto, &hints, &res)!=0)
+    if(rv = getaddrinfo(NULL, puerto, &hints, &res)!=0)
+        return -1;
+    
+    for(p=res;p!=NULL;p = p->ai_next)
     {
-        fprintf(stderr, "geatddrinfo: %s \n", gai_strerror(rv));
-        return 1;
-    }//llena res
-	
-    for(p=res; p!=NULL; p = p->ai_next)
-    {
-        if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
-        {
-            perror("server: socket");
-            continue;
-        }
-        if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-        {
-            perror("server: socket");
-            continue;
-            exit(1);
-        }
-	    if(bind(sockfd, res->ai_addr, res->ai_addrlen)== -1) // asociamos el puerto con el programa
-        {
-            close(sockfd);
-            perror("server:bind");
-            continue;
-        }
+        sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+        if(bind(sock,res->ai_addr, res->ai_addrlen) == -1)
+            close(sock);
         break;
     }
-    //
-    //sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol); //HACEMOS EL SOCKET QUE VAMOS A ESCUCHAR
-    
-	listen(sockfd, BACKLOG); // escuchamos al puerto que asociamos
+    freeaddrinfo(res);
+        
+    return sock;
+}
 
+int main(int argc, char *argv[])
+{
+//SOCKFD socket escucha, newfd socket conexion, len mensaje
+// their_add direccion de cliente, res resultado deb addrinfo
+    int sockfd,  new_fd, len, bytes_sent;  
+    char *msg, *puerto;
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+    //hints seteado a 0, puerto igualado a lo enviado en cli
+    puerto = argv[1];
+
+
+    sockfd = socketbind(puerto);
+   
+    listen(sockfd, BACKLOG); // escuchamos al puerto que asociamos
     printf("Buscando conexiones\n");
 //	while(1)
     {
@@ -64,7 +57,7 @@ int main(int argc, char *argv[])
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size); //reconocemos la existencia del cliente.
     
         printf("supuestamente aceptamos desde\n");	
-	    if(!fork())
+        if(!fork())
         {
             close(sockfd);
             if(send(new_fd, "Hello, World", 13, 0) == -1)
@@ -77,6 +70,5 @@ int main(int argc, char *argv[])
 
     //shutdown(sockfd,2);
     //shutdown(new_fd,2);
-    freeaddrinfo(res);
     return 0;
 }
